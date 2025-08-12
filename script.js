12701da1576b66f47594d70be85a644f055e3485
Omar Aldabbas <omaraldabbassjo@gmail.com>
2025-08-12 08:34:22 -0700

    Smooth scrolling

--- script.js
+++ script.js
@@ -85,3 +85,21 @@ function animate() {
 }
 
 animate();
+
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    const targetID = this.getAttribute('href').substring(1);
+    const targetElement = document.getElementById(targetID);
+
+    if (targetElement) {
+      targetElement.scrollIntoView({ behavior: 'smooth' });
+    }
+  });
+});
+
+
+
+
